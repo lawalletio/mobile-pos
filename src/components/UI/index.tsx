77db96022e714ebdd215b5fb26984de1773bec44
Jona <llamasjonat@gmail.com>
2023-10-25 04:04:16 -0300

    chore: remove elements

--- src/components/UI/index.tsx
+++ src/components/UI/index.tsx
@@ -1,4 +1,3 @@
-export { default as Alert } from './Alert'
 export { default as Button } from './Button'
 export { default as ButtonGroup } from './Button/ButtonGroup'
 export { default as Divider } from './Divider'
