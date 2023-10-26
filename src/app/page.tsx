ded3517dca16080da5726df63623163e7e09d5bb
Jona <llamasjonat@gmail.com>
2023-10-26 15:39:49 -0300

    fix(page): new structure for Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,48 @@
 'use client'
 
-import { useContext } from 'react'
-import { useRouter } from 'next/navigation'
-import { PantheonIcon } from '@bitcoin-design/bitcoin-icons-react/filled'
-
-import { LaWalletContext } from '@/context/LaWalletContext'
-import { formatToPreference } from '@/lib/formatter'
-
+import Link from 'next/link'
 import {
-  Flex,
-  Heading,
-  Text,
-  Divider,
-  Button,
-  Keyboard,
-  Icon
-} from '@/components/UI'
+  PantheonIcon,
+  SharedWalletIcon
+} from '@bitcoin-design/bitcoin-icons-react/filled'
+
+import { Flex, Heading, Text, Divider, Icon, Card } from '@/components/UI'
 import Container from '@/components/Layout/Container'
-import Navbar from '@/components/Layout/Navbar'
-import TokenList from '@/components/TokenList'
-import { useNumpad } from '@/hooks/useNumpad'
 
 export default function Home() {
-  const router = useRouter()
-
-  const { userConfig } = useContext(LaWalletContext)
-  const numpadData = useNumpad(userConfig.props.currency)
-
-  const handleClick = () => {
-    // POC
-    router.push('/payment')
-  }
-
   return (
     <>
-      <Navbar>
-        <Icon>
-          <PantheonIcon />
-        </Icon>
-        <Heading as="h5">Modo CAJA</Heading>
-      </Navbar>
       <Container size="small">
         <Divider y={24} />
         <Flex direction="column" gap={8} flex={1} justify="center">
-          <Flex justify="center" align="center" gap={4}>
-            {userConfig.props.currency !== 'SAT' && <Text>$</Text>}
-            <Heading>
-              {formatToPreference(
-                userConfig.props.currency,
-                numpadData.intAmount[numpadData.usedCurrency]
-              )}
-            </Heading>
+          <Heading as="h4">Selecciona un modo:</Heading>
+          <Flex gap={8}>
+            <Card color="secondary">
+              <Link href="/tree">
+                <Icon>
+                  <SharedWalletIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Arbolito</Heading>
+                  <Text size="small">
+                    Transferi dinero de una tarjeta a otra.
+                  </Text>
+                </Flex>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="/paydesk">
+                <Icon>
+                  <PantheonIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Caja</Heading>
+                  <Text size="small">Medio de cobro para tu negocio.</Text>
+                </Flex>
+              </Link>
+            </Card>
           </Flex>
-          <TokenList />
         </Flex>
-        <Divider y={24} />
-        <Flex gap={8}>
-          <Button onClick={handleClick}>Generar</Button>
-        </Flex>
-        <Divider y={24} />
-        <Keyboard numpadData={numpadData} />
         <Divider y={24} />
       </Container>
     </>
