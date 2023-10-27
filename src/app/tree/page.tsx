68d5378e3f1c851549d2c038bab143cdbf06906a
Agustin Kassis <webmaster@masize.com>
2023-10-27 13:51:01 -0300

    implemented lnurl on tree

--- src/app/tree/page.tsx
+++ src/app/tree/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useContext, useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { SharedWalletIcon } from '@bitcoin-design/bitcoin-icons-react/filled'
 
 import { LaWalletContext } from '@/context/LaWalletContext'
@@ -20,19 +20,88 @@ import Container from '@/components/Layout/Container'
 import Navbar from '@/components/Layout/Navbar'
 import TokenList from '@/components/TokenList'
 import { useNumpad } from '@/hooks/useNumpad'
+import { extractLNURLFromQR } from '@/lib/utils'
+import { useLN } from '@/context/LN'
+import { useNostr } from '@/context/Nostr'
+import { useOrder } from '@/context/Order'
 
 export default function Page() {
   const router = useRouter()
+  const { generateOrderEvent, setAmount, setOrderEvent, clear } = useOrder()
+  const { publish } = useNostr()
+  const query = useSearchParams()
+  const { fetchLNURL } = useLN()
 
   const { userConfig } = useContext(LaWalletContext)
   const numpadData = useNumpad(userConfig.props.currency)
 
   const [cardScanned, setCardScanned] = useState<boolean>(false)
 
-  const handleClick = () => {
+  const processUrl = useCallback(
+    async (url: string) => {
+      const lnurl = extractLNURLFromQR(url)
+
+      if (!lnurl) {
+        alert('Invalid QR code')
+        return
+      }
+
+      try {
+        await fetchLNURL(lnurl)
+        setCardScanned(true)
+      } catch (e) {
+        console.error(e)
+        alert(JSON.stringify(e))
+      }
+    },
+    [fetchLNURL]
+  )
+
+  const sats = numpadData.intAmount['SAT']
+
+  const handleClick = async () => {
     // POC
-    router.push('/payment')
+    const order = generateOrderEvent!()
+
+    console.dir(order)
+    // console.info('Publishing order')
+
+    publish!(order).catch(e => {
+      console.warn('Error publishing order')
+      console.warn(e)
+    })
+    setOrderEvent!(order)
+
+    router.push('/payment/' + order.id)
   }
+
+  useEffect(() => {
+    if (numpadData.usedCurrency !== userConfig.props.currency)
+      numpadData.modifyCurrency(userConfig.props.currency)
+  }, [numpadData, userConfig.props.currency])
+
+  useEffect(() => {
+    setAmount(sats)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sats])
+
+  useEffect(() => {
+    const url = query.get('data')
+
+    if (!url) {
+      return
+    }
+
+    console.info('processUrl?')
+    processUrl(url)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
+
+  // on mount
+  useEffect(() => {
+    clear()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
