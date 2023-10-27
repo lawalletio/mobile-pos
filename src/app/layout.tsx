f42900672ebf2c14638e1e46ed78102eda917a98
Jona <llamasjonat@gmail.com>
2023-10-27 16:23:40 -0300

    fix(layout): inset metadata on Home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,15 @@ const Providers = (props: ProviderProps) => {
     >
       <head>
         <title>{APP_NAME}</title>
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#1C1C1C" />
+
+        <link rel="manifest" href="/manifest.json" />
       </head>
 
       <body>
