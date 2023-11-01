62e8ae378b3e6959416d7a456458f3cb7ea59e94
Agustin Kassis <webmaster@masize.com>
2023-11-01 14:38:56 -0300

    change alert to console log on missing printer

--- src/hooks/usePrint.ts
+++ src/hooks/usePrint.ts
@@ -12,7 +12,7 @@ export const usePrint = (): PrintReturns => {
   const print = useCallback(
     (order: PrintOrder) => {
       if (!isAvailable) {
-        alert('No se puede imprimir, no se encuentra el objeto Android')
+        console.error('No se puede imprimir, no se encuentra el objeto Android')
         return
       }
       window!.Android!.print(JSON.stringify(order))
