b99f6380d822a2667c48bccce5649d6cfb85e1ab
Agustin Kassis <webmaster@masize.com>
2023-11-01 14:36:41 -0300

    added global types for Android

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,9 +2,15 @@ import React from 'react'
 
 declare global {
   interface Window {
+    injectedNFC: {
+      handleRead: (str: string) => void
+      handleError: (reason: string) => void
+      resolveFn?: (str: string) => void
+      rejectFn?: (reason?: string) => void
+    }
     Android?: {
       print: (str: string) => void
-      read: () => string
+      isNFCAvailable: () => boolean
     }
   }
 }
