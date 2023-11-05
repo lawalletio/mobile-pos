19705433ff449c1d10297d60d62b2f8445fb71ba
Agustin Kassis <webmaster@masize.com>
2023-11-05 16:44:00 -0300

    added isValidUrl function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -77,3 +77,16 @@ export const aggregateProducts = (
 
   return Array.from(productMap.values())
 }
+
+export function isValidUrl(urlString: string): boolean {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name and extension
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+  return !!pattern.test(urlString)
+}
