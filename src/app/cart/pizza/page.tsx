e661f75a37fe3bc96671d4cb616d2b3d8bfe0190
Agustin Kassis <webmaster@masize.com>
2023-11-09 16:07:25 -0300

    fix pizza

--- src/app/cart/pizza/page.tsx
+++ src/app/cart/pizza/page.tsx
@@ -2,7 +2,7 @@ import Page from '../page'
 import lud06 from '@/constants/lud06/pizza.json'
 
 export const PizzaMenu = () => {
-  return <Page name="green" title="Pizza Food" lud06={lud06} />
+  return <Page name="pizza" title="Pizza Food" lud06={lud06} />
 }
 
 export default PizzaMenu
