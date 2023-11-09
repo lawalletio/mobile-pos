82b9675df71180198798c8dcdae93a72ee2a2415
Agustin Kassis <webmaster@masize.com>
2023-11-09 09:48:20 -0300

    added types

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -1,53 +1,46 @@
 export interface InfoResponse {
   tag: 'laWallet:info'
-  status: {
-    initialized: boolean
-    associated: boolean
-    activated: boolean
-    hasDelegation: boolean
-  }
-  ntag424:
-    | {
-        ok: {
-          cid: string
-          ctr: number
-          ctrNew: number
-          otc: string | null
-          design: {
-            uuid: string
-            name: string
-          }
-        }
-      }
-    | { error: string }
-    | null
-  card:
-    | {
-        ok: {
+  info: {
+    status: {
+      initialized: boolean
+      associated: boolean
+      activated: boolean
+      hasDelegation: boolean
+    }
+    ntag424: {
+      ok: {
+        cid: string
+        ctr: number
+        ctrNew: number
+        otc: string | null
+        design: {
           uuid: string
           name: string
-          description: string
-          enabled: boolean
         }
       }
-    | { error: string }
-    | null
-  holder:
-    | {
-        ok: {
-          pubKey: string
-          delegations: {
-            kind: number | null
-            since: string
-            until: string
-            isCurrent: boolean
-            delegationConditions: string
-            delegationToken: string
-          }[]
-        }
+    } | null
+    card: {
+      ok: {
+        uuid: string
+        name: string
+        description: string
+        enabled: boolean
       }
-    | { error: string }
-    | null
+    } | null
+    holder: {
+      ok: {
+        pubKey: string
+        delegations: {
+          kind: number | null
+          since: string
+          until: string
+          isCurrent: boolean
+          delegationConditions: string
+          delegationToken: string
+        }[]
+      }
+    } | null
+  }
 }
 
 export interface ResetResponse {
