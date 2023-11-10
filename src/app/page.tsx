e642ff68f7d847ec38ecf0fd0820ab690a35f069
Agustin Kassis <webmaster@masize.com>
2023-11-09 22:55:49 -0300

    added cart icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,67 @@ export default function Page() {
         <Flex direction="column" gap={8} flex={1} justify="center">
           <Heading as="h4">Selecciona un modo:</Heading>
           <Flex gap={8}>
+            <Card>
+              <Link href="/cart/pizza">
+                <Icon>
+                  <CartIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Pizza</Heading>
+                  <Text size="small">Pizza Store</Text>
+                </Flex>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="/cart/green">
+                <Icon>
+                  <CartIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Urban Store</Heading>
+                  <Text size="small">Urbano</Text>
+                </Flex>
+              </Link>
+            </Card>
+          </Flex>
+
+          <Flex gap={8}>
+            <Card>
+              <Link href="/cart/coffee">
+                <Icon>
+                  <CartIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Cafe</Heading>
+                  <Text size="small">Coffee Store</Text>
+                </Flex>
+              </Link>
+            </Card>
+            <Card>
+              <Link href="/cart/green">
+                <Icon>
+                  <CartIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Green Shop</Heading>
+                  <Text size="small">Shop Green</Text>
+                </Flex>
+              </Link>
+            </Card>
+          </Flex>
+
+          <Flex gap={8}>
+            <Card>
+              <Link href="/paydesk">
+                <Icon>
+                  <PantheonIcon />
+                </Icon>
+                <Flex direction="column" gap={4}>
+                  <Heading as="h5">Caja</Heading>
+                  <Text size="small">Medio de cobro para tu negocio.</Text>
+                </Flex>
+              </Link>
+            </Card>
             <Card color="secondary">
               <Link href="/tree">
                 <Icon>
@@ -34,32 +95,9 @@ export default function Page() {
                 </Flex>
               </Link>
             </Card>
-            <Card>
-              <Link href="/paydesk">
-                <Icon>
-                  <PantheonIcon />
-                </Icon>
-                <Flex direction="column" gap={4}>
-                  <Heading as="h5">Caja</Heading>
-                  <Text size="small">Medio de cobro para tu negocio.</Text>
-                </Flex>
-              </Link>
-            </Card>
           </Flex>
+
           <Flex gap={8}>
-            <Card>
-              <Link href="/cart">
-                <Icon>
-                  <CartIcon />
-                </Icon>
-                <Flex direction="column" gap={4}>
-                  <Heading as="h5">Carrito de compras</Heading>
-                  <Text size="small">
-                    Empeza a vender productos seleccionados.
-                  </Text>
-                </Flex>
-              </Link>
-            </Card>
             <Card color="error">
               <Link href="/admin">
                 <Icon>
