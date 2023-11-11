d4d390e337faba00097a7fddda06344c1ee8234d
Agustin Kassis <webmaster@masize.com>
2023-11-10 22:37:36 -0300

    menu fix

--- src/app/cart/pizza/page.tsx
+++ src/app/cart/pizza/page.tsx
@@ -2,7 +2,7 @@ import lud06 from '@/constants/lud06/pizza.json'
 import Menu from '../components/menu'
 
 const PizzaMenu = () => {
-  return <Menu name="coffee" title="Pizza Store" lud06={lud06} />
+  return <Menu name="pizza" title="Pizza Store" lud06={lud06} />
 }
 
 export default PizzaMenu
