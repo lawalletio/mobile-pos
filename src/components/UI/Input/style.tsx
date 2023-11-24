ea5760058398bbc1fb525ff279101322d81d1cb4
Agustin Kassis <webmaster@masize.com>
2023-11-24 20:34:04 -0300

    added Input component

---
+++ src/components/UI/Input/style.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { styled } from 'styled-components'
+
+export const InputCustom = styled.input`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  min-width: 40px;
+  min-height: 40px;
+
+  padding: 4px 6px;
+
+  border: none;
+  border-radius: 10px;
+
+  font-size: 0.9em;
+  font-weight: 700;
+  text-align: center;
+
+  width: 100%;
+`
