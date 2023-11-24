ba3a423a597b7ff20dd1eb56e57e9bbc620f47a2
Agustin Kassis <webmaster@masize.com>
2023-11-24 18:07:46 -0300

    added context

--- src/context/LaWalletContext.tsx
+++ src/context/LaWalletContext.tsx
@@ -3,18 +3,26 @@ import { createContext } from 'react'
 
 // Context and Hooks
 import useConfiguration, { ConfigReturns } from '@/hooks/useConfiguration'
+import { useLocalStorage } from 'react-use-storage'
 
 interface LaWalletContextType {
   userConfig: ConfigReturns
+  destionationLUD06: string
+  setDestionationLUD06: (value: string) => void
 }
 
+// Constants
 export const LaWalletContext = createContext({} as LaWalletContextType)
 
 export function LaWalletProvider({ children }: { children: React.ReactNode }) {
   const userConfig: ConfigReturns = useConfiguration()
+  const [destionationLUD06, setDestionationLUD06] =
+    useLocalStorage<string>('destionationLUD06')
 
-  const value = {
-    userConfig
+  const value: LaWalletContextType = {
+    userConfig,
+    destionationLUD06,
+    setDestionationLUD06
   }
 
   return (
