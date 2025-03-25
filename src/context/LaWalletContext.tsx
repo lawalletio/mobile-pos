4631d25ca0a6e242e5b09bf68a1196113a67996a
Agustin Kassis <webmaster@masize.com>
2025-03-25 17:38:14 -0300

    add default lud06

--- src/context/LaWalletContext.tsx
+++ src/context/LaWalletContext.tsx
@@ -1,6 +1,8 @@
 // React/Next
 import { createContext } from 'react'
 
+import posResponse from '../constants/lud06/pos.json'
+
 // Context and Hooks
 import useConfiguration, { ConfigReturns } from '@/hooks/useConfiguration'
 import { useLocalStorage } from 'react-use-storage'
@@ -18,7 +20,7 @@ export const LaWalletContext = createContext({} as LaWalletContextType)
 export function LaWalletProvider({ children }: { children: React.ReactNode }) {
   const userConfig: ConfigReturns = useConfiguration()
   const [destinationLUD06, setDestinationLUD06] =
-    useLocalStorage<LNURLResponse>('destinationLUD06')
+    useLocalStorage<LNURLResponse>('destinationLUD06', posResponse)
 
   const value: LaWalletContextType = {
     userConfig,
